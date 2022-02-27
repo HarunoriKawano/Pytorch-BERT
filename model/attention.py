Fix(attention.py, transformer.py, utils.py): Fixed layer normalization

model/attention.py
@@ -3,8 +3,6 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
-from model.utils import LayerNorm
-
 
 class AttentionLayer(nn.Module):
 
@@ -12,7 +10,7 @@ class AttentionLayer(nn.Module):
         super(AttentionLayer, self).__init__()
         self.self_attention = MultiHeadAttention(feature_size=feature_size, head_num=head_num, dropout_rate=0.1)
         self.feed_forward = AttentionFeedForward(feature_size=feature_size, dropout_rate=0.1)
-        self.layer_norm = LayerNorm(feature_size=feature_size)
+        self.layer_norm = nn.LayerNorm(feature_size)
 
     def forward(self, inputs, mask=None):
         q = k = v = inputs

model/transformer.py
@@ -3,7 +3,6 @@ import torch
 from torch import nn
 
 from model.attention import AttentionLayer
-from model.utils import LayerNorm
 
 
 class TransformerLayer(nn.Module):
@@ -12,7 +11,7 @@ class TransformerLayer(nn.Module):
 
         self.attention_layer = AttentionLayer(feature_size, attention_head_num)
         self.feed_forward = TransformerFeedForward(feature_size, hidden_size)
-        self.layer_norm = LayerNorm(feature_size)
+        self.layer_norm = nn.LayerNorm(feature_size)
 
     def forward(self, inputs, mask=None):
         attention = self.attention_layer(inputs, mask)